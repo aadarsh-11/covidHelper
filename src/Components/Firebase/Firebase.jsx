15c744e16b869346cb1ed5b10fc0477a7df5e825
Harsh2220 <harshgajjar145@gmail.com>
2021-04-25 15:20:51 +0530

    Firebase

---
+++ src/Components/Firebase/Firebase.jsx
@@ -0,0 +1,15 @@
+import Firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDjIrLH_6jbCOL3yNg8EnrRUWIM85PPrmY",
+    authDomain: "covidhelper-3e894.firebaseapp.com",
+    projectId: "covidhelper-3e894",
+    storageBucket: "covidhelper-3e894.appspot.com",
+    messagingSenderId: "342848145626",
+    appId: "1:342848145626:web:4c1cbe4b13a0bf0a7cfde3",
+    measurementId: "G-DRE3XCG7RX"
+  };
+
+const config = firebase.initializeApp(firebaseConfig);
+
+const 
