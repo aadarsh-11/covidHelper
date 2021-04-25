090b37a9082e65a4ec2b179ade15c07cf87690c9
Harsh2220 <harshgajjar145@gmail.com>
2021-04-25 16:08:07 +0530

    Form

--- src/Components/Firebase/Firebase.jsx
+++ src/Components/Firebase/Firebase.jsx
@@ -1,6 +1,6 @@
-import Firebase from 'firebase';
+import firebase from 'firebase';
 
-var firebaseConfig = {
+var firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyDjIrLH_6jbCOL3yNg8EnrRUWIM85PPrmY",
     authDomain: "covidhelper-3e894.firebaseapp.com",
     projectId: "covidhelper-3e894",
@@ -8,8 +8,8 @@ var firebaseConfig = {
     messagingSenderId: "342848145626",
     appId: "1:342848145626:web:4c1cbe4b13a0bf0a7cfde3",
     measurementId: "G-DRE3XCG7RX"
-  };
+});
 
-const config = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
 
-const 
+export { db };
--- src/Components/Forms/Form.jsx
+++ src/Components/Forms/Form.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import { React, useState } from "react";
 import './Form.css';
+import { db } from '../Firebase/Firebase';
 import { Checkbox, Button } from '@material-ui/core';
 
 const Form = () => {
+
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+
+  const handleSubmit = (e) => {
+    // e.preventdefault();
+
+    db.collection('Information').add({
+      Username: name,
+      Useremail: email,
+    })
+      .then(() => {
+        alert("Form has been submitted");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    setname("");
+    setemail("");
+  }
+
   return (
     <div className="container">
 
       <div className="details">
-        <input id="name" className="input" type="text" placeholder="Enter Your Name"></input>
-        <input id="email" className="input" type="text" placeholder="Enter Your Email"></input>
+        <input id="name" className="input" type="text" placeholder="Enter Your Name" onChange={(e) => setname(e.target.value)}></input>
+        <input id="email" className="input" type="text" placeholder="Enter Your Email" onChange={(e) => setemail(e.target.value)}></input>
       </div>
 
       <div className="requirements">
@@ -45,7 +67,7 @@ const Form = () => {
       </div>
 
       <div className="submit">
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
           Submit
         </Button>
       </div>
