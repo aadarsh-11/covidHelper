e4ab355f658de2ecfbf3e3f3b9ccec5b8103e222
Harsh2220 <harshgajjar145@gmail.com>
2021-04-25 16:27:52 +0530

    Footer

--- src/Components/Dashboard/Home.jsx
+++ src/Components/Dashboard/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Action from "./Action";
+import Footer from "./Footer";
 import "./Home.css";
 import Message from "./Message";
 import PeopleInfo from "./PeopleInfo";
@@ -10,6 +11,7 @@ const Home = () => {
       <Message />
       <Action />
       <PeopleInfo peopleData={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]} />
+      <Footer/>
     </React.Fragment>
   );
 };
