1b81d90f15518c898da5b748da0a23e8cfd233f8
Harsh2220 <harshgajjar145@gmail.com>
2021-04-25 11:52:28 +0530

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,17 +18,10 @@ const Navbar = () => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-<<<<<<< HEAD
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
             <a class="nav-link active" href="#">
-=======
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
->>>>>>> 3c3df4c187f92b9404e2fe91693ca1d5bb64ebe4
               Home
             </a>
           </li>
