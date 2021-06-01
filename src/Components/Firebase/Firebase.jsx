35757b55f66b86e781473b89ed130cbda1c4c2b4
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2021-06-01 11:21:48 +0530

    Update Firebase.jsx

--- src/Components/Firebase/Firebase.jsx
+++ src/Components/Firebase/Firebase.jsx
@@ -1,15 +1,5 @@
 import firebase from 'firebase';
 
-var firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDjIrLH_6jbCOL3yNg8EnrRUWIM85PPrmY",
-    authDomain: "covidhelper-3e894.firebaseapp.com",
-    projectId: "covidhelper-3e894",
-    storageBucket: "covidhelper-3e894.appspot.com",
-    messagingSenderId: "342848145626",
-    appId: "1:342848145626:web:4c1cbe4b13a0bf0a7cfde3",
-    measurementId: "G-DRE3XCG7RX"
-});
-
 const db = firebaseApp.firestore();
 
 export default db;
